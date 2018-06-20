e4d44a5a1c666ebe9b63c5c6291b6e759b0d95e3
Peter Cai <peter@typeblog.net>
2018-06-20 16:48:08 +0800

    enable LTO & strip final binary & use upx to compress

--- build-static.sh
+++ build-static.sh
@@ -9,7 +9,7 @@ if [ ! -d temp/openssl ]; then
   mkdir $PREFIX
   pushd openssl
   git checkout OpenSSL_1_1_0h
-  CC="musl-gcc -fPIE -pie" LDFLAGS="-L/usr/lib" CFLAGS="-I/usr/lib/musl/include" ./Configure --prefix=$PREFIX linux-x86_64 no-shared no-async
+  CC="musl-gcc -fPIE -pie" LDFLAGS="-L/usr/lib" CFLAGS="-flto -I/usr/lib/musl/include" ./Configure --prefix=$PREFIX linux-x86_64 no-shared no-async
   make && make install
   popd
   popd
@@ -17,3 +17,10 @@ fi
 rm -rf target/release
 rm -rf target/x86_64-unknown-linux-musl
 OPENSSL_STATIC=1 OPENSSL_DIR=$PWD/temp/openssl-shared cargo build --release --target=x86_64-unknown-linux-musl
+rm -rf release
+mkdir -p release
+cp target/x86_64-unknown-linux-musl/release/tws-rust release/
+echo "Stripping the final binary"
+strip release/tws-rust
+echo "Compressing the binary using upx"
+upx --brute release/tws-rust
